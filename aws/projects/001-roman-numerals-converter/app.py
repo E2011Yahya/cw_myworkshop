8a0844e2aeaae6cba4ae4c5a822eee555f1b3910
İyianlar <iyianlar@Iyianlar-MacBook-Pro.local>
2020-09-20 11:34:21 +0300

    app.py yülendi

---
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
+if __name__ == '__main__':
+    #app.run(debug = True)
+    app.run(host='0.0.0.0', port=80)
