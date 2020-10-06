556f73bb05f1c4f295cd679c9939f84c056f09cd
İyianlar <iyianlar@Iyianlar-MacBook-Pro.local>
2020-10-06 18:29:38 +0300

    passwordgen and miliseconds project added

--- python/coding-challenges/cc-005-create-phonebook/phonebook.py
+++ python/coding-challenges/cc-005-create-phonebook/phonebook.py
@@ -11,16 +11,27 @@ while 0 < int(select) < 4 :
         if name in book :
             x = book[name]
             print(x)
+            print()
         else:
             print(f"{name} is not in the address book") 
-        
+            print()
 
     if int(select) == 2:
         name = input("Insert name of the person : ")
-        num = input("Insert phone number of the person: ")
-        book[name] = num
-        print(f"Phone number of {name} is inserted into the phonebook")
-        print(book)
+        if name in book :
+            print("This name is already in phonebook")
+            print()
+        else:
+            num = input("Insert phone number of the person: ")
+            if num.isdecimal():
+                book[name] = num
+                print(f"Phone number of {name} is inserted into the phonebook")
+                print()
+            else: 
+                print("Invalid input format, cancelling operation")
+                print()
+
+       # print(book)
 
     if int(select) == 3:
         name = input("Whom to delete from phonebook : ")
@@ -28,12 +39,10 @@ while 0 < int(select) < 4 :
         if name in book :
             book.pop(name)
             print(f"{name} is deleted from the phonebook")
-
+            print()
         else:
             print(f"{name} is not in the address book")
-
-
-
+            print()
 
     select = input("Welcome to the phonebook application \n 1. Find phone number \n 2. Insert a phone number \n 3. Delete a person from the phonebook \n 4. Terminate \n Select operation on Phonebook App (1/2/3) :")    
     
